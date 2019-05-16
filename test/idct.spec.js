e309bf2671c708a2af54695fef11d49a33cda379
Naokazu Shimabukuro <shimabukuro@pnrm.co.jp>
2019-05-16 20:11:52 +0900

    rename

--- test/idct.spec.js
+++ test/idct.spec.js
@@ -4,13 +4,6 @@ const PI = Math.PI;
 
 describe('idct', () => {
 
-  before(() => {
-
-  });
-
-  after(() => {
-  });
-
   it('idct', () => {
     const expecteds = [15 / 16 * PI, 13 / 16 * PI, 11 / 16 * PI, 9 / 16 * PI, 7 / 16 * PI, 5 / 16 * PI, 3 / 16 * PI, 1 / 16 * PI];
     const actuals = idct(dct(expecteds));
